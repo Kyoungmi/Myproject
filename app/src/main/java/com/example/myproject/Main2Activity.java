fd7ba91f7681bd7ce6fc9f4cadc3bcf7fa541d10
김경미 <kkongmi@kkongmis-MacBook-Pro.local>
2019-05-16 16:03:52 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myproject/Main2Activity.java
@@ -0,0 +1,21 @@
+package com.example.myproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        EditText idText = (EditText) findViewById(R.id.idText);
+        EditText passwordText = (EditText) findViewById(R.id.passwordText);
+        TextView welcomeMessage = (TextView) findViewById(R.id.welcomeMessage);
+
+    }
+}
---
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText idText = (EditText) findViewById(R.id.idText);
+        EditText passwordText = (EditText) findViewById(R.id.passwordText);
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        TextView registerButton = (TextView) findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent registerintent = new Intent(MainActivity.this, RegisterActivity.class);
+                MainActivity.this.startActivity(registerintent);
+
+            }
+        });
+
+    }
+}
